270cc6e5f60af65f113b8e8d8848780fe5b4d970
Karolos <karolos.kantounis@gmail.com>
2020-07-15 10:44:07 -0400

    Added logic to distinguish left and right clicks

--- PysimpleGUIExample.py
+++ PysimpleGUIExample.py
@@ -6,25 +6,46 @@ game = MinesweeperBoard()
 
 layout =  [[sg.Button('?', size=(4, 2), key=(i,j), pad=(0,0)) for j in range(game.column_size)] for i in range(game.row_size)]
 
-window = sg.Window('Minesweeper', layout)
+window = sg.Window('Minesweeper', layout, finalize=True)
+
+
+# this is to capture right clicks ( crazy ... )
+for r in range(game.row_size):
+    for c in range(game.column_size):
+        window[(r,c)].bind("<Button-2>","Right")  # <----- this for windows might be Button-3
+
 show_everything = False
 while True:
     event, values = window.read()
+
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    # window[(row, col)].update('New text')   # To change a button's text, use this pattern
-    game.players_choice_of_tile_and_action(event, "reveal")
+
+    is_right_click = event[1]=="Right"
+
+    if is_right_click:
+
+        # right click logic
+
+        continue # <--- this is needed for now otherwise left click logic will also happen
+
+    # a change since right click even is ((r,c),"Right") left is just (r,c)
+    resolved_event = event if not is_right_click else event[0]
+
+    game.players_choice_of_tile_and_action(resolved_event, "reveal")
     if game.game_over():
         show_everything = True
-        if game.human_wins:
-            sg.popup_ok('WINNER!!')
-        else:
-            sg.popup_ok('LOSER!!!')
+
     for r in range (game.row_size):
         for c in range (game.column_size):
             if show_everything or game.board[r][c].is_revealed:
                 window[(r,c)].update(game.board[r][c].board_value(), button_color=('white','black'))
 
+    if game.game_over():
+        if game.human_wins:
+            sg.popup_ok('WINNER!!')
+        else:
+            sg.popup_ok('LOSER!!!')
 
 
 
