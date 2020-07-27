a3f5ed41a25822232e9b0cf40bf306b87201b971
Alkantou <alexkantounis@gmail.com>
2020-07-27 16:17:49 +0300

    Minesweeper continuation - Save File, Load File, Solved disabled buttons, added color to numbers and fixed the toggling of flags

--- PysimpleGUIExample.py
+++ PysimpleGUIExample.py
@@ -5,13 +5,25 @@ import time
 from playsound import playsound
 from threading import Thread
 
+COLOR_MAP = {0: 'white', 1: 'green', 2: 'purple', 3: 'blue', 4: 'brown', 5: 'orange', 6: 'grey', 7: 'pink', 8:'yellow'}
+
 
 def my_playsound(filename):
     if not muted:
-
         my_thread = Thread(target=playsound, args=(filename,))
         my_thread.start()
 
+
+def determine_number_color(number: int) -> str:
+    return COLOR_MAP[number]
+
+
+def paint_board(game, window):
+    for r in range(game.row_size):
+        for c in range(game.column_size):
+            if game.board[r][c].is_revealed:
+                window[(r, c)].update(game.board[r][c].board_value(), disabled=True, button_color=('white', 'black'),
+                                      disabled_button_color=(determine_number_color(game.board[r][c].number_of_neighbour_bombs), 'black'))
 
 
 def load_minesweeper_window(difficulty: Difficulty = Difficulty.EASY, load_from_file: bool = False):
@@ -28,10 +40,7 @@ def load_minesweeper_window(difficulty: Difficulty = Difficulty.EASY, load_from_
         for c in range(game.column_size):
             window[(r, c)].bind("<Button-2>", "Right")  # <----- this for windows might be Button-3
             window[(r, c)].bind("<Button-3>", "Right")  # <----- this for windows might be Button-3
-    for r in range(game.row_size):
-        for c in range(game.column_size):
-            if game.board[r][c].is_revealed:
-                window[(r, c)].update(game.board[r][c].board_value(), button_color=('white', 'black'))
+    paint_board(game, window)
     return (game, window)
 
 
@@ -40,7 +49,8 @@ game, window = load_minesweeper_window(load_from_file=True)
 # this is to capture right clicks
 
 startime = int(round(time.time() * 100))
-muted = False
+muted = True
+
 while True:
     event, values = window.read(timeout=10)
     if event == '__TIMEOUT__':
@@ -64,7 +74,7 @@ while True:
         if pop_up_event == 'Yes':
             window.close()
             game, window = load_minesweeper_window(difficulty=Difficulty[event.upper()])
-            startime = int(round(time.time()*100))
+            startime = int(round(time.time() * 100))
         pop_up_window.close()
 
         continue
@@ -72,14 +82,21 @@ while True:
     coordinates = event if not is_right_click else event[0]
     if is_right_click:
         # right click logic
-        window[coordinates].update(u'\u2713',button_color=('red', '#283b5b'))
+        if game.board[coordinates[0]][coordinates[1]].is_revealed:
+            continue
+        tile_text = window[coordinates].ButtonText
+        if tile_text == "?":
+            window[coordinates].update(u'\u2713', button_color=('red', '#283b5b'))
+        else:
+            window[coordinates].update('?', button_color=('white', '#283b5b'))
 
         continue  # <--- this is needed for now otherwise left click logic will also happen
 
     # a change since right click even is ((r,c),"Right") left is just (r,c)
-
+    tile_text = window[coordinates].ButtonText
+    if tile_text == u'\u2713':
+        continue
     game.players_choice_of_tile_and_action(coordinates, "reveal")
-    window[coordinates].Disabled = True
     if game.game_over():
         if game.human_wins:
             sg.popup_ok('WINNER!!')
@@ -90,9 +107,6 @@ while True:
             my_playsound("mal.mp3")
     else:
         my_playsound("bravo.mp3")
-    for r in range(game.row_size):
-        for c in range(game.column_size):
-            if game.board[r][c].is_revealed:
-                window[(r, c)].update(game.board[r][c].board_value(), button_color=('white', 'black'))
+    paint_board(game, window)
 
 window.close()
